bce3c78259e37ba5a66e419efcf29f9ed8ee9367
Jayowen323542 <323542@student.mboutrecht.nl>
2019-05-14 10:00:16 +0200

    Create bestel.php

---
+++ bestel.php
@@ -0,0 +1,39 @@
+<form>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputEmail4">Email</label>
+      <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+    </div>
+    <div class="form-group col-md-6">
+      <label for="inputPassword4">Voornaam</label>
+      <input type="text" class="form-control" id="inputfname" placeholder="Pieter">
+    </div>
+    <div class="form-group col-md-6">
+      <label for="inputPassword4">Tussenvoegsel</label>
+      <input type="text" class="form-control" id="inputinfix" placeholder="van de">
+    </div>
+    <div class="form-group col-md-6">
+      <label for="inputPassword4">Achternaam</label>
+      <input type="text" class="form-control" id="inputsname" placeholder="Koning">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="inputAddress">Straat</label>
+    <input type="text" class="form-control" id="inputAddress" placeholder="Straatweg">
+  </div>
+  <div class="form-group">
+    <label for="inputAddress2">Huisnummer</label>
+    <input type="text" class="form-control" id="inputAddress2" placeholder="123">
+  </div>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputCity">Stad</label>
+      <input type="text" class="form-control" id="inputCity" placeholder="Amsterdam">
+    </div>
+    <div class="form-group col-md-2">
+      <label for="inputZip">Postcode</label>
+      <input type="text" class="form-control" id="inputZip" placeholder="1111AA">
+    </div>
+  </div>
+  <button type="submit" class="btn btn-primary">Bestel</button>
+</form>
