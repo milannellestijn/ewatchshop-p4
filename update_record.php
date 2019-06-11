027e632f5213e61aa2bdb153c835b8e5eb6a2413
Jayowen323542 <323542@student.mboutrecht.nl>
2019-06-11 10:18:41 +0200

    Create update_record.php

---
+++ update_record.php
@@ -0,0 +1,23 @@
+<?php
+ 
+
+    include("./connect_db.php");
+
+    include("./functions.php");
+
+    $id = sanitize($_POST["id"]);
+
+    $email = sanitize($_POST["email"]) ;
+    $userrole = sanitize($_POST["userrole"]) ;
+
+    $sql = "UPDATE `login` SET 
+                    `email` = '$email', 
+                    `userrole` = '$userrole',
+            WHERE `id` = $id";
+
+    mysqli_query($conn, $sql);
+    
+
+    header("Refresh: 4; ./index.php?content=gebruikersrollen");
+    
+?>
