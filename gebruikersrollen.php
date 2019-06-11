662b0c745e02dc1c2bb2842767d4fedddf67d14f
Jayowen323542 <323542@student.mboutrecht.nl>
2019-06-11 10:18:49 +0200

    Create gebruikersrollen.php

---
+++ gebruikersrollen.php
@@ -0,0 +1,68 @@
+<?php
+//We maken contact met de mysql-server
+include("./connect_db.php");
+
+// We includen de sanitize funtion om de $_POST waarden schoon te maken.
+include("./functions.php");
+
+// Dit is een select query om de records uit de tabel te halen
+$sql = "SELECT * FROM `login` WHERE userrole !='root'";
+
+// vuur de query af op de database en je krijgt antwoord terug. stop dit $result
+$result = mysqli_query($conn, $sql);
+
+
+
+
+
+?>
+    <main class="container">
+
+    <div class="row">
+     <div class="col-12">
+            <table class="table table-hover">
+        <thead>
+            <tr>
+            <th scope="col">id</th>
+            <th scope="col">Email</th>
+            <th scope="col">Role</th>
+            <th scope="col">Keuzen1</th>
+            <th scope="col">Keuzen2</th>
+
+
+            </tr>
+        </thead>
+
+        <tbody>
+            <?php
+                        // $resultis niet leesbaar, we maken er een associatief array van
+                        //var_dump($result);
+                        while ($record = mysqli_fetch_assoc($result)) {
+                        echo "<tr><th scope = 'row'>" . $record["id"] . "</th>" .
+                     "<td>" . $record["email"] . "</td>" .
+                     "<td>" . $record["userrole"] . "</td>" .
+                     "<td>" . $record["keuzen1"] . "</td>" .
+                     "<td>" . $record["keuzen2"] . "</td>" .
+
+
+
+                    "<td>
+                        <a href='./update.php?id=". $record["id"] ."'>
+                        <img src='./img/b_edit.png' alt='edit' style='width: 20px; height: 20px;'>
+                        </a>
+                        </td>
+                     <td>
+                        <a href='./delete.php?id=". $record["id"] ."'>
+                        <img src='./img/b_drop.png' alt='drop' style='width: 20px; height: 20px;'>
+                        </a>
+                        </td>
+                         </tr>";
+                         }
+            ?>
+        </tbody>
+</table>
+
+     </div>
+    </div>
+
+    </main>
