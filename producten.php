2f8888e3dfc02318016e0b19133eff2037d70289
Jayowen323542 <323542@student.mboutrecht.nl>
2019-06-11 12:55:18 +0200

    Update producten.php

--- producten.php
+++ producten.php
@@ -27,3 +27,14 @@ $product = $db_handle->runQuery("SELECT * FROM `product` ");
 		}
 	}
 	?>
+
+<form action="./add.php" method="post">
+<div class="card" style="width: 18rem;">
+<input type="text" class="form-control" id="name" name="name"  >
+<input type="text" class="form-control" id="code" name="code"  >
+<input type="text" class="form-control" id="price" name="price"  >
+<input type="text" class="form-control" id="description" name="description"  >
+<input type="text" class="form-control" id="image" name="image"  >
+<button type="submit" class="btn btn-primary">Stuur op</button>
+</div>
+</form>
