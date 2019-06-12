ddb8e4989287863ec9114e54f24209bf7c2faa18
Jayowen323542 <323542@student.mboutrecht.nl>
2019-06-12 16:17:24 +0200

    Update product_add.php

--- product_add.php
+++ product_add.php
@@ -1,10 +1,15 @@
-<form action="./add.php" method="post">
-<div class="card" style="width: 18rem;">
-<input type="text" class="form-control" id="name" name="name"  >
-<input type="text" class="form-control" id="code" name="code"  >
-<input type="text" class="form-control" id="price" name="price"  >
-<input type="text" class="form-control" id="description" name="description"  >
-<input type="text" class="form-control" id="image" name="image"  >
+<form action="./product_add-script.php" method="post">
+<div class="card" style="width: 36rem;">
+<label for="name">Name</label>
+<input type="text" class="form-control" id="name" name="name" placeholder="Name"  >
+<label for="name">Code</label>
+<input type="text" class="form-control" id="code" name="code" placeholder="SM000"  >
+<label for="name">Price</label>
+<input type="text" class="form-control" id="price" name="price" placeholder="100,00" >
+<label for="name">Description</label>
+<input type="text" class="form-control" id="description" name="description" placeholder="description"  >
+<label for="name">Images</label>
+<input type="text" class="form-control" id="image" name="image" placeholder="./pictures/x.jpg" >
 <button type="submit" class="btn btn-primary">Stuur op</button>
 </div>
 </form>
