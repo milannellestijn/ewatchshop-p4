311b1eaeedc9914f4b6cad8018e720c1797367fb
Jayowen323542 <323542@student.mboutrecht.nl>
2019-06-12 15:42:17 +0200

    Create update_product.php

---
+++ update_product.php
@@ -0,0 +1,29 @@
+
+
+<?php
+var_dump($_POST);
+ include("./connect_db.php");
+
+ include("./functions.php");
+
+
+$id = sanitize($_GET["id"]);
+
+ $sql = "SELECT * FROM `product` WHERE `id` = $id";
+
+ $result = mysqli_query($conn, $sql);
+
+ $record = mysqli_fetch_assoc($result);
+
+
+?>
+<form action="./update_producten-script.php" method="post">
+    <input type="hidden" name="id" value="<?=$record['id']?>">
+<input type="text" class="form-control" id="name" name="name" value="<?php echo $record["name"]; ?>" >
+<input type="text" class="form-control" id="code" name="code" value="<?php echo $record["code"]; ?>" >
+<input type="text" class="form-control" id="price" name="price" value="<?php echo $record["price"]; ?>" >
+<input type="text" class="form-control" id="description" name="description" value="<?php echo $record["description"]; ?>" >
+<input type="text" class="form-control" id="image" name="image" value="<?php echo $record["image"]; ?>" >
+
+    <button type="submit" class="btn btn-primary">Verander</button>
+</form>
