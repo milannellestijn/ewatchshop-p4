64a6bd82b70baa13201dc3c26fa85b4c27fa51be
Jayowen323542 <323542@student.mboutrecht.nl>
2019-06-12 15:42:37 +0200

    Create product_add-script.php

---
+++ product_add-script.php
@@ -0,0 +1,29 @@
+<?php
+ 
+
+    include("./connect_db.php");
+
+    include("./functions.php");
+
+    
+    $name = sanitize($_POST["name"]) ;
+    $code = sanitize($_POST["code"]) ;
+    $description = sanitize($_POST["description"]) ;
+    $price = sanitize($_POST["price"]);
+    $image = sanitize($_POST["image"]);
+
+
+    $sql = "INSERT INTO `product` (
+                    `id`
+                    `name`, 
+                    `code`,
+                    `image`, 
+                    `price` ,
+                    `description`) VALUES (NULL, '$name', '$code', '$image', '$price', '$description')";
+
+    mysqli_query($conn, $sql);
+    
+
+    header("Refresh: 2; ./index.php?content=producten");
+    
+?>
