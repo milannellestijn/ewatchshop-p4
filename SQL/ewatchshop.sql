4e1f848212b76962ceecd4ba3fdbb23be3311029
Jayowen323542 <323542@student.mboutrecht.nl>
2019-06-18 09:32:14 +0200

    Update ewatchshop.sql

--- SQL/ewatchshop.sql
+++ SQL/ewatchshop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Gegenereerd op: 11 jun 2019 om 07:41
+-- Gegenereerd op: 18 jun 2019 om 07:31
 -- Serverversie: 5.7.23
 -- PHP-versie: 7.2.10
 
@@ -38,15 +38,14 @@ CREATE TABLE IF NOT EXISTS `contact` (
   `onderwerp` varchar(50) NOT NULL,
   `vraag` varchar(4000) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `contact`
 --
 
 INSERT INTO `contact` (`id`, `voornaam`, `tussenvoegsel`, `achternaam`, `email`, `onderwerp`, `vraag`) VALUES
-(1, 'nmkwm', 'fd', 'wadfsg', 'nwdjij@dewd', 'wsdaesfgrt', 'wadsf'),
-(2, 'nmkwm', 'fd', 'wadfsg', 'nwdjij@dewd', 'wsdaesfgrt', 'wadsf');
+(4, 'Henk', '', 'Hansen', 'vraag@vraag.com', 'Vraag', 'Wat is dit ?');
 
 -- --------------------------------------------------------
 
@@ -56,20 +55,109 @@ INSERT INTO `contact` (`id`, `voornaam`, `tussenvoegsel`, `achternaam`, `email`,
 
 DROP TABLE IF EXISTS `login`;
 CREATE TABLE IF NOT EXISTS `login` (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `iduser` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(350) NOT NULL,
   `password` varchar(60) NOT NULL,
   `userrole` enum('administrator','customer') NOT NULL DEFAULT 'customer',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `firstname` varchar(100) DEFAULT NULL,
+  `infix` varchar(20) DEFAULT NULL,
+  `lastname` varchar(200) DEFAULT NULL,
+  `phone` int(15) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `postalcode` varchar(6) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`iduser`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `login`
 --
 
-INSERT INTO `login` (`id`, `email`, `password`, `userrole`) VALUES
-(1, 'milan_nellestijn@hotmail.com', '$2y$10$EQZghE4/L0D4n9VewPnjaOVZS20dJ9X2R111OqnRzdUCfkMEpuG/y', 'administrator'),
-(2, 'admin@admin.com', '$2y$10$if.rKneSN1mf72huDPyefOSc0kOpHVT07V8WzceggOiUS0wx2bzfu', 'administrator');
+INSERT INTO `login` (`iduser`, `email`, `password`, `userrole`, `firstname`, `infix`, `lastname`, `phone`, `address`, `postalcode`, `city`) VALUES
+(13, '123@123', '$2y$10$qkA/2e5D9.MzEtCiSFpOsuMiODJ7jxcDefyrbBrfWVmrxyt3sjQBu', 'administrator', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(15, 'admin@admin', '$2y$10$bO7LUjw4iW4G2pFeH1aqdOhSgx4Qhp4GxqY2a/vZ2JQt1c1kGVDAi', 'administrator', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(17, 'klant@klant', '$2y$10$aew6gXr4QZTbwwuHVXkN9eW.1MQBtdBG8uhAFsWBqri1wRoEGGM2y', 'customer', NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `order`
+--
+
+DROP TABLE IF EXISTS `order`;
+CREATE TABLE IF NOT EXISTS `order` (
+  `idorder` int(11) NOT NULL AUTO_INCREMENT,
+  `iduser` int(11) NOT NULL,
+  `date` datetime NOT NULL,
+  `price_ex` decimal(5,2) NOT NULL,
+  `price_inc` decimal(5,2) NOT NULL,
+  `status` enum('onderweg','afgeleverd','betaalt','gesorteerd') NOT NULL,
+  PRIMARY KEY (`idorder`),
+  KEY `iduser` (`iduser`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `orderline`
+--
+
+DROP TABLE IF EXISTS `orderline`;
+CREATE TABLE IF NOT EXISTS `orderline` (
+  `idorderline` int(11) NOT NULL AUTO_INCREMENT,
+  `idorder` int(11) NOT NULL,
+  `idporduct` int(11) NOT NULL,
+  `amount` int(11) NOT NULL,
+  `price` decimal(4,2) NOT NULL,
+  `total` decimal(5,2) NOT NULL,
+  PRIMARY KEY (`idorderline`),
+  KEY `idorder` (`idorder`,`idporduct`),
+  KEY `idporduct` (`idporduct`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE IF NOT EXISTS `product` (
+  `idproduct` int(8) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `code` varchar(255) NOT NULL,
+  `image` text NOT NULL,
+  `price` double(10,2) NOT NULL,
+  `description` text NOT NULL,
+  `stock` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idproduct`),
+  UNIQUE KEY `product_code` (`code`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `product`
+--
+
+INSERT INTO `product` (`idproduct`, `name`, `code`, `image`, `price`, `description`, `stock`) VALUES
+(15, 'De eerste smartwatch', 'SM001', './pictures/smartwatch1.jpg', 100.00, 'DE beste smartwatch', 0),
+(16, 'Giga smartwatch', 'sm100', '', 999.00, 'De giga smartwatch ', 0);
+
+--
+-- Beperkingen voor geëxporteerde tabellen
+--
+
+--
+-- Beperkingen voor tabel `order`
+--
+ALTER TABLE `order`
+  ADD CONSTRAINT `order_ibfk_1` FOREIGN KEY (`iduser`) REFERENCES `login` (`iduser`);
+
+--
+-- Beperkingen voor tabel `orderline`
+--
+ALTER TABLE `orderline`
+  ADD CONSTRAINT `orderline_ibfk_1` FOREIGN KEY (`idorder`) REFERENCES `order` (`idorder`),
+  ADD CONSTRAINT `orderline_ibfk_2` FOREIGN KEY (`idporduct`) REFERENCES `product` (`idproduct`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
