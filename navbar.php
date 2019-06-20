95b740c3aed5b7e700f72d019a9c5a67438a17c3
Jayowen323542 <323542@student.mboutrecht.nl>
2019-06-20 13:01:09 +0200

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -70,7 +70,7 @@ if(!empty($_SESSION["shopping_cart"])) {
 $cart_count = count(array_keys($_SESSION["shopping_cart"]));
 ?>
 <div class="cart_div">
-<a href="index.php"><img src="./pictures/shopcart.png" hight="42" width="42"/> Cart<span>
+<a href="index.php?content=shopcart"><img src="./pictures/shopcart.png" hight="42" width="42"/> Cart<span>
 <?php echo $cart_count; ?></span></a>
 </div>
 <?php
